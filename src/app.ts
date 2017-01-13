5d5c01ce5af3368728c6eb315d10b240ae17e243
Michael Kaaden <github@kaaden.net>
2017-01-13 16:07:44 +0100

    Use favicon.ico

--- src/app.ts
+++ src/app.ts
@@ -22,7 +22,7 @@ export class App {
         this.app.set("views", path.join(__dirname, "../views"));
         this.app.set("view engine", "pug");
 
-        // this.app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
+        this.app.use(favicon(path.join(__dirname, "public", "favicon.ico")));
         this.app.use(logger("dev"));
         this.app.use(bodyParser.json());
         this.app.use(bodyParser.urlencoded({extended: false}));
